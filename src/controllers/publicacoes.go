3f3d7457933e2fad41ebad2b59325689fef6b8e7
Felipe Bezerra Joazeiro <felipejoazeiro@gmail.com>
2026-01-28 10:20:27 -0300

    Página de atualização da publicação

--- src/controllers/publicacoes.go
+++ src/controllers/publicacoes.go
@@ -7,8 +7,10 @@ import (
 	"net/http"
 	"strconv"
 	"webapp/src/config"
+	"webapp/src/modelos"
 	"webapp/src/requisicoes"
 	"webapp/src/respostas"
+	"webapp/src/utils"
 
 	"github.com/gorilla/mux"
 )
@@ -77,6 +79,68 @@ func DescurtirPublicacao(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicações/%d/descurtir", config.ApiUrl, publicacaoId)
 
 	res, err := requisicoes.FazerReqComAuth(r, http.MethodPost, url, nil)
+	if err != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroApi{Mensagem: err.Error()})
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, res)
+		return
+	}
+	respostas.JSON(w, res.StatusCode, nil)
+}
+
+func CarregarPaginaDeEdicaoDePublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroApi{Mensagem: "ID da publicação inválido"})
+		return
+	}
+
+	url := fmt.Sprintf("%s/publicações/%d", config.ApiUrl, publicacaoID)
+
+	res, err := requisicoes.FazerReqComAuth(r, http.MethodGet, url, nil)
+	if err != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroApi{Mensagem: err.Error()})
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, res)
+		return
+	}
+	var publicacao modelos.Publicacao
+	if err := json.NewDecoder(res.Body).Decode(&publicacao); err != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroApi{Mensagem: "Erro ao ler a publicação"})
+		return
+	}
+	utils.ExecutarTemplate(w, "editar-publicacao.html", publicacao)
+}
+
+func EditarPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoId, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroApi{Mensagem: "ID da publicação inválido"})
+		return
+	}
+	r.ParseForm()
+	publicacao, err := json.Marshal(map[string]string{
+		"titulo":   r.FormValue("titulo"),
+		"conteudo": r.FormValue("conteudo"),
+	})
+	if err != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroApi{Mensagem: err.Error()})
+		return
+	}
+	url := fmt.Sprintf("%s/publicações/%d/editar", config.ApiUrl, publicacaoId)
+
+	res, err := requisicoes.FazerReqComAuth(r, http.MethodPut, url, bytes.NewBuffer(publicacao))
+
 	if err != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroApi{Mensagem: err.Error()})
 		return
--- src/router/rotas/publicacoes.go
+++ src/router/rotas/publicacoes.go
@@ -1,24 +1,33 @@
 package rotas
 
-import "webapp/src/controllers"
+import (
+	"net/http"
+	"webapp/src/controllers"
+)
 
 var rotasPublicacoes = []Rota{
 	{
 		URI:                "/publicacoes",
-		Metodo:             "POST",
+		Metodo:             http.MethodPost,
 		Funcao:             controllers.CriarPublicacao,
 		RequerAutenticacao: true,
 	},
 	{
 		URI:                "/publicacoes/{publicacaoId}/curtir",
-		Metodo:             "POST",
+		Metodo:             http.MethodPost,
 		Funcao:             controllers.CurtirPublicacao,
 		RequerAutenticacao: true,
 	},
 	{
 		URI:                "/publicacoes/{publicacaoId}/descurtir",
-		Metodo:             "POST",
+		Metodo:             http.MethodPost,
 		Funcao:             controllers.DescurtirPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		URI:                "/publicacoes/{publicacaoId}/editar",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.CarregarPaginaDeEdicaoDePublicacao,
 		RequerAutenticacao: true,
 	},
 }

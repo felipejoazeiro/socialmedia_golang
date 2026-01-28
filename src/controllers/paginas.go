dbedf6dd6b4dfc1393a6f29e4296b86c777a6637
Felipe Bezerra Joazeiro <felipejoazeiro@gmail.com>
2026-01-28 14:28:44 -0300

    Redirecionamento caso já logado

--- src/controllers/paginas.go
+++ src/controllers/paginas.go
@@ -11,10 +11,18 @@ import (
 	"webapp/src/requisicoes"
 	"webapp/src/respostas"
 	"webapp/src/utils"
+
+	"github.com/gorilla/mux"
 )
 
 // Renderiza a tela de login
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Ler(r)
+
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
+		return
+	}
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
@@ -44,7 +52,6 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 
 	cookie, _ := cookies.Ler(r)
 	usuarioId, _ := strconv.ParseUint(cookie["id"], 10, 64)
-	
 
 	utils.ExecutarTemplate(w, "home.html", struct {
 		Publicacoes []modelos.Publicacao
@@ -54,3 +61,34 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 		UsuarioID:   usuarioId,
 	})
 }
+
+// Renderiza a página de edição de publicação
+func CarregarPaginaDeEdicaoDePublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroApi{Mensagem: "ID da publicação inválido"})
+		return
+	}
+
+	url := fmt.Sprintf("%s/publicações/%d", config.ApiUrl, publicacaoID)
+
+	res, err := requisicoes.FazerReqComAuth(r, http.MethodGet, url, nil)
+	if err != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroApi{Mensagem: err.Error()})
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, res)
+		return
+	}
+	var publicacao modelos.Publicacao
+	if err := json.NewDecoder(res.Body).Decode(&publicacao); err != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroApi{Mensagem: "Erro ao ler a publicação"})
+		return
+	}
+	utils.ExecutarTemplate(w, "editar-publicacao.html", publicacao)
+}
+
--- src/controllers/publicacoes.go
+++ src/controllers/publicacoes.go
@@ -7,10 +7,8 @@ import (
 	"net/http"
 	"strconv"
 	"webapp/src/config"
-	"webapp/src/modelos"
 	"webapp/src/requisicoes"
 	"webapp/src/respostas"
-	"webapp/src/utils"
 
 	"github.com/gorilla/mux"
 )
@@ -90,35 +88,6 @@ func DescurtirPublicacao(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	respostas.JSON(w, res.StatusCode, nil)
-}
-
-func CarregarPaginaDeEdicaoDePublicacao(w http.ResponseWriter, r *http.Request) {
-	parametros := mux.Vars(r)
-	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
-	if erro != nil {
-		respostas.JSON(w, http.StatusBadRequest, respostas.ErroApi{Mensagem: "ID da publicação inválido"})
-		return
-	}
-
-	url := fmt.Sprintf("%s/publicações/%d", config.ApiUrl, publicacaoID)
-
-	res, err := requisicoes.FazerReqComAuth(r, http.MethodGet, url, nil)
-	if err != nil {
-		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroApi{Mensagem: err.Error()})
-		return
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode >= 400 {
-		respostas.TratarStatusCodeErro(w, res)
-		return
-	}
-	var publicacao modelos.Publicacao
-	if err := json.NewDecoder(res.Body).Decode(&publicacao); err != nil {
-		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroApi{Mensagem: "Erro ao ler a publicação"})
-		return
-	}
-	utils.ExecutarTemplate(w, "editar-publicacao.html", publicacao)
 }
 
 func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
